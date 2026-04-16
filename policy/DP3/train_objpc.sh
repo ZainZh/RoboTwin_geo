3502e65cace1b6e89757405c27c48302628c3dcf
Zain <1155169521@link.cuhk.edu.hk>
2026-04-16 15:19:46 +0800

    update resume

--- policy/DP3/train_objpc.sh
+++ policy/DP3/train_objpc.sh
@@ -5,7 +5,8 @@ task_config=${2}
 expert_data_num=${3}
 seed=${4}
 gpu_id=${5}
-object_placeholders=${6:-\{A\},\{B\}}
+resume=${6:-true}
+object_placeholders=${7:-\{A\},\{B\}}
 
 if [ ! -d "./data/${task_name}-${task_config}-${expert_data_num}-objpc.zarr" ]; then
     bash process_data_objpc.sh "${task_name}" "${task_config}" "${expert_data_num}" "${object_placeholders}"
@@ -27,6 +28,7 @@ python train_dp3.py --config-name=robot_dp3_objpc.yaml \
     task_name=${task_name} \
     hydra.run.dir=${run_dir} \
     training.debug=${DEBUG} \
+    training.resume=${resume} \
     training.seed=${seed} \
     training.device="cuda:0" \
     exp_name=${exp_name} \
